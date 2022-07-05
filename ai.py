e1f81011da307b0f7fb511e7fe4ba9f1c71b8045
Devin Chen <taunwe79@gmail.com>
2022-07-05 15:43:58 -0500

    wrote methods to make a gesture

--- ai.py
+++ ai.py
@@ -1,3 +1,16 @@
-class Ai:
+from player import Player
+import random
+
+class Ai(Player):
     def __init__(self):
-        pass
+        super().__init__()
+
+    def choose_gesture(self):
+        random.choice(self.gestures)
+        print(random.choice(self.gestures))
+
+    def chose_gesture(self):
+        print(self.chose_gesture)
+
+        self.chose_gesture()
+    
--- human.py
+++ human.py
@@ -1,3 +1,19 @@
-class Human:
+from player import Player
+
+class Human(Player):
     def __init__(self):
-        pass
+        super().__init__()
+
+    def choose_gesture(self):
+        print(f'1.{self.gestures[0]} 2.{self.gestures[1]} 3.{self.gestures[2]} 4.{self.gestures[3]} 5.{self.gestures[4]}')
+        user_input = int(input('Make a choice: '))
+        if user_input > len(self.gestures):
+            self.choose_gesture()
+        gesture = self.gestures[user_input - 1]
+        return gesture 
+        
+    def chose_gesture(self):
+        print(self.choose_gesture())
+
+human = Human()
+human.chose_gesture()
