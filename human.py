ef3e7e7c26b180898b55bfd556509bb916dd676a
unknown <mbisovskyi@gmail.com>
2022-07-06 19:37:25 -0400

    Bugs fixed

--- human.py
+++ human.py
@@ -5,9 +5,11 @@ class Human(Player):
         super().__init__()
 
     def picked_gesture(self, player_name):
-        print(f'\n1.{self.gestures[0]}   2.{self.gestures[1]}   3.{self.gestures[2]}   4.{self.gestures[3]}   5.{self.gestures[4]}')
-        user_input = int(input(f'\n{player_name} makes choice: '))
-        if user_input > len(self.gestures):
-            self.picked_gesture()
-        gesture = self.gestures[user_input - 1]
-        return gesture
+        while True:
+            print(f'\n1.{self.gestures[0]}   2.{self.gestures[1]}   3.{self.gestures[2]}   4.{self.gestures[3]}   5.{self.gestures[4]}')
+            user_input = int(input(f'\n{player_name} makes choice: '))
+            if user_input > len(self.gestures):
+                continue
+            else:
+                gesture = self.gestures[user_input - 1]
+                return gesture
